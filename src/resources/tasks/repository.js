784b6a1bef3f93f20e6be04b37ddc0a40e639832
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-12-05 21:05:39 +0300

    Add task resource validation

--- src/resources/tasks/repository.js
+++ src/resources/tasks/repository.js
@@ -1,3 +1,5 @@
+import { number, object, string } from 'yup'
+import { validate } from '../../common/validation'
 import Task from './model'
 
 export function findAll(boardId) {
@@ -8,14 +10,33 @@ export function findById(boardId, taskId) {
   return Task.query().findOne({ id: taskId, boardId }).throwIfNotFound({ message: 'Task not found' })
 }
 
-export function create(boardId, data) {
-  return Task.query().insert({ ...data, boardId })
+export async function create(boardId, data) {
+  const schema = object({
+    title: string().required().min(2),
+    order: number().required().integer(),
+    description: string().min(6),
+    userId: string().nullable().uuid(),
+    boardId: string().nullable().uuid(),
+    columnId: string().nullable().uuid()
+  })
+  const validData = await validate(schema, data)
+
+  return Task.query().insert({ ...validData, boardId })
 }
 
 export async function update(boardId, taskId, data) {
+  const schema = object({
+    title: string().required().min(2),
+    order: number().required().integer(),
+    description: string().min(6),
+    userId: string().nullable().uuid(),
+    boardId: string().nullable().uuid(),
+    columnId: string().nullable().uuid()
+  })
+  const validData = await validate(schema, data)
   const task = await Task.query().findOne({ id: taskId, boardId }).throwIfNotFound({ message: 'Task not found' })
 
-  return task.$query().updateAndFetch(data)
+  return task.$query().updateAndFetch(validData)
 }
 
 export function del(boardId, taskId) {
