be815e6a970c67bf8e55374cd8938f5a8a06bbb1
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-12-05 20:41:46 +0300

    Add user resource validation

--- src/resources/users/repository.js
+++ src/resources/users/repository.js
@@ -1,3 +1,5 @@
+import { object, string } from 'yup'
+import { validate } from '../../common/validation'
 import User from './model'
 
 export function findAll() {
@@ -8,12 +10,26 @@ export function findById(userId) {
   return User.query().findById(userId).throwIfNotFound({ message: 'User not found' })
 }
 
-export function create(data) {
-  return User.query().insert(data)
+export async function create(data) {
+  const schema = object({
+    name: string().required().min(2),
+    login: string().min(4),
+    password: string().min(8)
+  })
+  const validData = await validate(schema, data)
+
+  return User.query().insert(validData)
 }
 
-export function update(userId, data) {
-  return User.query().updateAndFetchById(userId, data).throwIfNotFound({ message: 'User not found' })
+export async function update(userId, data) {
+  const schema = object({
+    name: string().required().min(2),
+    login: string().min(4),
+    password: string().min(8)
+  })
+  const validData = await validate(schema, data)
+
+  return User.query().updateAndFetchById(userId, validData).throwIfNotFound({ message: 'User not found' })
 }
 
 export async function del(userId) {
