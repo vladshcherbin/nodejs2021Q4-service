20dd0ae8e4b82cbfea987eeeb5f646745e78e726
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-12-19 10:36:43 +0300

    Add tsdoc eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,9 @@ module.exports = {
   extends: '@shcherbin/eslint-config/node-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  plugins: ['eslint-plugin-tsdoc'],
+  rules: {
+    'tsdoc/syntax': 'warn'
   }
 }
