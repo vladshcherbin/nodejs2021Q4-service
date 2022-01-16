dca5dfa4a16739a83b5520ed02357e254ee0993f
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2022-01-16 23:02:07 +0300

    Add task relations

--- src/resources/tasks/model.ts
+++ src/resources/tasks/model.ts
@@ -1,4 +1,6 @@
 import typeorm from 'typeorm'
+import Board from '../boards/model'
+import User from '../users/model'
 
 /**
  * Task database model.
@@ -25,6 +27,12 @@ class Task extends typeorm.BaseEntity {
 
   @typeorm.Column('uuid', { nullable: true })
     columnId?: string
+
+  @typeorm.ManyToOne(() => Board, (board) => board.id)
+    board?: Board
+
+  @typeorm.ManyToOne(() => User, (user) => user.id)
+    user?: User
 }
 
 export default Task
