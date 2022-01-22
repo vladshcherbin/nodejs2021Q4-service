9b2683b379bb7890dbd8e67aaa3e91582501b5b3
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2022-01-22 15:06:21 +0300

    Update tasks table with snake case columns

--- migrations/20220116152101_create_tasks_table.js
+++ migrations/20220116152101_create_tasks_table.js
@@ -1,15 +1,15 @@
 export function up(knex) {
   return knex.schema.createTable('tasks', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+    table.uuid('board_id')
+    table.uuid('column_id')
+    table.uuid('user_id')
     table.string('title').notNullable()
     table.integer('order')
     table.string('description')
-    table.uuid('userId')
-    table.uuid('boardId')
-    table.uuid('columnId')
 
-    table.foreign('userId').references('users.id').onUpdate('CASCADE').onDelete('SET NULL')
-    table.foreign('boardId').references('boards.id').onUpdate('CASCADE').onDelete('CASCADE')
+    table.foreign('user_id').references('users.id').onUpdate('CASCADE').onDelete('SET NULL')
+    table.foreign('board_id').references('boards.id').onUpdate('CASCADE').onDelete('CASCADE')
   })
 }
 
